a49381412d1ee18f6fb670693db8a342eb78c7ae
aksrajvanshi <aksrajvanshi@gmail.com>
2019-09-17 22:28:02 -0400

    First commit

---
+++ src/main/java/com/app/yumdrop/YumdropApplication.java
@@ -0,0 +1,13 @@
+package com.app.yumdrop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YumdropApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YumdropApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/yumdrop/YumdropApplicationTests.java
@@ -0,0 +1,16 @@
+package com.app.yumdrop;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class YumdropApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
