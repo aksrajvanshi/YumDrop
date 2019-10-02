9aef641391566dfc0e3598a549a5eae7b5f2ab5a
aksrajvanshi <aksrajvanshi@gmail.com>
2019-10-02 18:00:59 -0400

    Procfile and application.properties plus static files loaded

--- src/main/java/com/app/yumdrop/YumDropController.java
+++ src/main/java/com/app/yumdrop/YumDropController.java
@@ -3,14 +3,13 @@ package com.app.yumdrop;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 
-@Controller
 @ComponentScan
+@Controller
 public class YumDropController {
 
-    @RequestMapping(value = "/", method = RequestMethod.GET)
+    @RequestMapping(value = "/publicpage")
     public String hello() {
         return "index";
     }
