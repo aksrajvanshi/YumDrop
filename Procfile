dc1867e0d347b28f7d5db3b1e2787a708bb8a664
aksrajvanshi <aksrajvanshi@gmail.com>
2019-10-02 13:10:19 -0400

    Trying to fix heroku deploy by changing application properties and Procfile

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: java -Dserver.port=8080 $JAVA_OPTS -jar target/yumdrop-0.0.1-SNAPSHOT.jar
+web: java $JAVA_OPTS -jar target/yumdrop-0.0.1-SNAPSHOT.jar --server.port=$PORT $JAR_OPTS
--- src/main/java/com/app/yumdrop/YumDropController.java
+++ src/main/java/com/app/yumdrop/YumDropController.java
@@ -2,11 +2,8 @@ package com.app.yumdrop;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 
 
 @Controller
