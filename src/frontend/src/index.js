b8f23ca91567322bc47e622daf558e82a45c318b
austKline <55597606+austKline@users.noreply.github.com>
2019-10-02 16:42:39 -0400

    Working popups

--- src/frontend/src/index.js
+++ src/frontend/src/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/App.jsx';
 import * as serviceWorker from './serviceWorker';
+import OtherPage from './components/OtherPage.jsx';
+import { Route, Link, BrowserRouter as Router } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const routing = (
+    <Router>
+        <div>
+            <Route exact path="/" component={App}/>
+            <Route path="/OtherPage" component={OtherPage} />
+        </div>
+    </Router>
+)
+
+ReactDOM.render(routing, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
