e1c4bdf4191708131a663bd6a3c8dd0e9de4e62c
aksrajvanshi <aksrajvanshi@gmail.com>
2019-10-04 20:20:26 -0400

    Fixed error with Controller and added html files in resources/static folder

--- src/main/java/com/app/yumdrop/YumDropController.java
+++ src/main/java/com/app/yumdrop/YumDropController.java
@@ -1,6 +1,5 @@
 package com.app.yumdrop;
 
-import com.app.yumdrop.Entity.Users;
 import com.app.yumdrop.Repository.UsersRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
@@ -16,10 +15,6 @@ import java.security.NoSuchAlgorithmException;
 @Controller
 public class YumDropController {
 
-<<<<<<< HEAD
-    @RequestMapping(value = "/")
-    public String hello() {
-=======
     @Autowired
     private UsersRepository userRepository;
 
@@ -35,7 +30,6 @@ public class YumDropController {
                 .printHexBinary(digest).toUpperCase();
 
         System.out.println(myHash + " length of hashed password: " + myHash.length());
->>>>>>> 6cdbe1550b6f9ed916be18cd197914214270e343
         return "index";
     }
 
--- src/main/resources/static/service-worker.js
+++ src/main/resources/static/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d4a96fa4f96646a763ced177d6fb501a.js"
+  "/precache-manifest.1c47984121eaadf2d36c8fcf8ff87a52.js"
 );
 
 self.addEventListener('message', (event) => {
