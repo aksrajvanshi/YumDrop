f7dad1ce88b0be8022c4413ea02cd054c982bc59
Murali Kammili <kmkvtab@gmail.com>
2019-10-06 11:37:27 -0400

    Facebook OAuth test implementaion

---
+++ spring-boot-facebook-signin/src/main/java/com/yumdrop/security/springbootfacebooksignin/SpringBootFacebookSigninApplication.java
@@ -0,0 +1,13 @@
+package com.yumdrop.security.springbootfacebooksignin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFacebookSigninApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootFacebookSigninApplication.class, args);
+	}
+
+}
