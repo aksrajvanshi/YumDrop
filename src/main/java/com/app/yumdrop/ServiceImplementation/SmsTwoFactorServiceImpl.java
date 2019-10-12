ea9f9a0a211557015e1239c412c206f1156f5dd9
aksrajvanshi <aksrajvanshi@gmail.com>
2019-10-12 11:47:46 -0400

    Hide details about Twilio account

--- src/main/java/com/app/yumdrop/ServiceImplementation/SmsTwoFactorServiceImpl.java
+++ src/main/java/com/app/yumdrop/ServiceImplementation/SmsTwoFactorServiceImpl.java
@@ -4,22 +4,24 @@ import com.app.yumdrop.Service.SmsTwoFactorService;
 import com.twilio.Twilio;
 import com.twilio.rest.api.v2010.account.Message;
 import com.twilio.type.PhoneNumber;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SmsTwoFactorServiceImpl implements SmsTwoFactorService {
 
-    private final static String ACCOUNT_SID = "";
-    private final static String AUTH_TOKEN = "";
+    private static final String ACCOUNT_SID = System.getenv("TWILIO_ACCOUNT_SID");
+    private static final String AUTH_TOKEN = System.getenv("TWILIO_AUTH_TOKEN");
 
-    static {
+    static{
         Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
     }
 
     @Override
     public boolean send2FaCodeAsSms(String mobilePhoneNumber, String twoFactorCode) {
+        System.out.println(ACCOUNT_SID);
 
-        Message.creator(new PhoneNumber(mobilePhoneNumber), new PhoneNumber("+16197802581"), " Your Two Factor Authentication Code is: " + twoFactorCode).create();
+        Message.creator(new PhoneNumber(mobilePhoneNumber), new PhoneNumber("+16197802581"), "Hello Dhriti Mehta! Your Two Factor Authentication Code is: " + twoFactorCode).create();
         return true;
     }
 }
