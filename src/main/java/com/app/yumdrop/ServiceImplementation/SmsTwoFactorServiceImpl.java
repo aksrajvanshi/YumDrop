43669fb076871bf2e34b4d5ec46aca5a6152251d
aksrajvanshi <aksrajvanshi@gmail.com>
2019-10-12 11:54:49 -0400

    Minor Refactoring to 2 factor auth code

--- src/main/java/com/app/yumdrop/ServiceImplementation/SmsTwoFactorServiceImpl.java
+++ src/main/java/com/app/yumdrop/ServiceImplementation/SmsTwoFactorServiceImpl.java
@@ -4,7 +4,6 @@ import com.app.yumdrop.Service.SmsTwoFactorService;
 import com.twilio.Twilio;
 import com.twilio.rest.api.v2010.account.Message;
 import com.twilio.type.PhoneNumber;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,15 +12,13 @@ public class SmsTwoFactorServiceImpl implements SmsTwoFactorService {
     private static final String ACCOUNT_SID = System.getenv("TWILIO_ACCOUNT_SID");
     private static final String AUTH_TOKEN = System.getenv("TWILIO_AUTH_TOKEN");
 
-    static{
+    static {
         Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
     }
 
     @Override
     public boolean send2FaCodeAsSms(String mobilePhoneNumber, String twoFactorCode) {
-        System.out.println(ACCOUNT_SID);
-
-        Message.creator(new PhoneNumber(mobilePhoneNumber), new PhoneNumber("+16197802581"), "Hello Dhriti Mehta! Your Two Factor Authentication Code is: " + twoFactorCode).create();
+        Message.creator(new PhoneNumber(mobilePhoneNumber), new PhoneNumber("+16197802581"), "Hello! Your Two Factor Authentication Code is: " + twoFactorCode).create();
         return true;
     }
 }
