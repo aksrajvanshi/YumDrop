71ae5dac12fc9be65af59def7b31ac16e3c78055
aksrajvanshi <aksrajvanshi@gmail.com>
2019-10-11 22:49:03 -0400

    Made the code modular and introduced service module. Added comments in the Controller class.

--- src/main/java/com/app/yumdrop/Controller/YumDropController.java
+++ src/main/java/com/app/yumdrop/Controller/YumDropController.java
@@ -1,15 +1,9 @@
 package com.app.yumdrop.Controller;
 
-import com.app.yumdrop.Entity.Users;
-import com.app.yumdrop.ExceptionHandler.CustomExceptionHandler;
 import com.app.yumdrop.FormEntity.UsersDetails;
-import com.app.yumdrop.Repository.UsersRepository;
-import com.app.yumdrop.Utils.PasswordUtils;
-import org.hibernate.exception.ConstraintViolationException;
+import com.app.yumdrop.Service.UserRegistrationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.http.HttpRequest;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,53 +11,48 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import javax.validation.ConstraintViolation;
-import java.sql.SQLException;
-
 
 @ComponentScan
 @Controller
 public class YumDropController {
 
     @Autowired
-    private UsersRepository userRepository;
+    UserRegistrationService userRegistrationService;
 
-    @Autowired
-    private CustomExceptionHandler exceptionHandler;
-
-
+    /**
+     * Loads the initial public page of the Yumdrop application
+     *
+     * @return
+     */
     @RequestMapping(value = "/")
     public String loadInitialPublicPage() {
-
-        String user = "akshay";
-        String email = "aksrajvanshi@gmail.com";
-        String password = "aksrajvanshi";
 
         return "index";
     }
 
-
+    /**
+     * Registers a user in the system.
+     *
+     * @param userDataForm
+     * @return
+     */
     @RequestMapping(value = "/userRegistration", method = RequestMethod.POST)
-    public ResponseEntity<?> userRegistration(@RequestBody UsersDetails userDataForm){
+    public ResponseEntity<?> userRegistration(@RequestBody UsersDetails userDataForm) {
 
-        boolean userExistsBool = userRepository.existsById(userDataForm.getUser_email());
-
-        if(userExistsBool){
-            throw new ConstraintViolationException("User already exists", new SQLException(" Insert query"), "user_email");
-        }
-
-        Users userToRegister = new Users(userDataForm.getUser_email(), userDataForm.getUser_name(),
-                userDataForm.getUser_phonenum(), "+1", PasswordUtils.convertToHash(userDataForm.getUserPassword()), "SYSTEM", "SYSTEM");
-
-        userRepository.save(userToRegister);
-        return ResponseEntity.ok().build();
+        return userRegistrationService.registerUser(userDataForm);
     }
 
+    /**
+     * registers a restaurant in the system.
+     *
+     * @param restaurantDataForm
+     * @return
+     */
     @PostMapping(value = "/restaurantRegistration")
-    public String testButton(@RequestBody String userDataForm) {
-        System.out.println(userDataForm);
+    public String restaurantRegistration(@RequestBody String restaurantDataForm) {
+        System.out.println(restaurantDataForm);
 
-        return userDataForm;
+        return restaurantDataForm;
     }
 
 
