708d6daf299a0f898fc79f61436a365f51e49a8e
Murali Kammili <kmkvtab@gmail.com>
2019-10-23 02:56:12 -0400

    Added entity, form entity and controller files

---
+++ src/main/java/com/app/yumdrop/Controller/UpdatePasswordController.java
@@ -0,0 +1,30 @@
+package com.app.yumdrop.Controller;
+
+import com.app.yumdrop.FormEntity.ForgotPasswordForm; //
+import com.app.yumdrop.FormEntity.UpdatePasswordForm;
+import com.app.yumdrop.Service.ForgotPasswordService; //
+import com.app.yumdrop.Service.UpdatePasswordService;
+import com.app.yumdrop.Utils.PasswordUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@ComponentScan
+@Controller
+public class UpdatePasswordController {
+
+    @Autowired
+    private UpdatePasswordService updatePasswordService;
+
+    @RequestMapping(value = "/setNewUserPassword", method = RequestMethod.POST)
+    public ResponseEntity<?> matchAndSetNewPassword(@RequestBody UpdatePasswordForm updatePasswordForm) {
+
+        return updatePasswordService.matchAndSetNewPassword(updatePasswordForm.getUserEmail(),
+                updatePasswordForm.getPassword(), PasswordUtils.convertPasswordToHash(updatePasswordForm.getPassword()));
+    }
+
+}
---
+++ src/main/java/com/app/yumdrop/FormEntity/UpdatePasswordForm.java
@@ -0,0 +1,56 @@
+package com.app.yumdrop.FormEntity;
+
+public class UpdatePasswordForm {
+
+    private String userEmail;
+    private String password;
+    private String confirmPassword;
+
+    public UpdatePasswordForm() {
+    }
+
+    public UpdatePasswordForm(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public UpdatePasswordForm(String userEmail, String password, String confirmPassword) {
+        this.userEmail = userEmail;
+        this.password = password;
+        this.confirmPassword = confirmPassword;
+    }
+
+
+    @Override
+    public String toString() {
+        return "UpdatePasswordForm{" +
+                "userEmail='" + userEmail + '\'' +
+                ", password='" + password + '\'' +
+                ", confirmPassword='" + confirmPassword + '\'' +
+                '}';
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+/*
+    public String getConfirmPassword() {
+        return confirmPassword;
+    }
+
+    public void setConfirmPassword(String confirmPassword) {
+        this.confirmPassword = confirmPassword;
+    }
+*/
+}
