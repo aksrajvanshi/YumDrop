6755d09a43c9242c3cc8676ad4c50806c9d89c46
Murali Kammili <kmkvtab@gmail.com>
2019-11-06 11:53:26 -0500

    Removed alerts

--- src/main/frontend/src/LoginForm.js
+++ src/main/frontend/src/LoginForm.js
@@ -86,12 +86,11 @@ class App extends Component {
             }),
         }).then(res => {
 
-            alert("Entered");
-            alert(res.status);
+           
             if (res.status !== 200) {
                 this.setState({redirect: true, userRegister: false});
                 this.forwardToLoginErrorPage();
-                alert("Hey going to Error page");
+               
             }else {
                 this.setState({redirect: true, userRegister: false, emailSelectForgotPassword: false, forgotPasswordSelect: true});
                 this.forwardToSuccessfullyChangedPasswordPage();
@@ -113,8 +112,7 @@ class App extends Component {
             }),
         }).then(res => {
 
-            alert("Entered");
-            alert(res.status);
+            
             if (res.status !== 200) {
                 this.setState({redirect: true, userRegister: false});
                 this.forwardToLoginErrorPage();
