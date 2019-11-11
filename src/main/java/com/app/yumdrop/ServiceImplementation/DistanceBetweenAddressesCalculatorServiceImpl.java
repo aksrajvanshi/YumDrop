e93a4023a8a1e8008c9f9441adcdcd8cceea0704
aksrajvanshi <aksrajvanshi@gmail.com>
2019-11-11 01:05:06 -0500

    Added functionality and dependency for calculating distance between 2 addresses.

---
+++ src/main/java/com/app/yumdrop/ServiceImplementation/DistanceBetweenAddressesCalculatorServiceImpl.java
@@ -0,0 +1,51 @@
+package com.app.yumdrop.ServiceImplementation;
+
+import com.app.yumdrop.Service.DistanceBetweenAddressesCalculatorService;
+import com.google.maps.DirectionsApi;
+import com.google.maps.DistanceMatrixApi;
+import com.google.maps.DistanceMatrixApiRequest;
+import com.google.maps.GeoApiContext;
+import com.google.maps.errors.ApiException;
+import com.google.maps.model.DistanceMatrix;
+import com.google.maps.model.TravelMode;
+import okhttp3.OkHttpClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+
+@Service
+public class DistanceBetweenAddressesCalculatorServiceImpl implements DistanceBetweenAddressesCalculatorService {
+
+    @Value("${google.maps.distanceapi.key}")
+    String googleMapsAPIKey;
+
+    OkHttpClient client = new OkHttpClient();
+
+    @Override
+    public void calculateDistance(String userAddress, String restaurantAddress) {
+
+        GeoApiContext context = new GeoApiContext.Builder()
+                .apiKey(googleMapsAPIKey)
+                .build();
+        DistanceMatrixApiRequest req = DistanceMatrixApi.newRequest(context);
+        DistanceMatrix result = null;
+        try {
+            result = req.origins(userAddress)
+                    .destinations(restaurantAddress)
+                    .mode(TravelMode.DRIVING)
+                    .avoid(DirectionsApi.RouteRestriction.TOLLS)
+                    .language("en-US")
+                    .await();
+        } catch (ApiException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(result.rows[0].elements[0].distance.inMeters + " -- " + result.rows[0].elements[0].distance.humanReadable);
+
+    }
+}
