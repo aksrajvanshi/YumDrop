887323606c2a91c24af15051d2ea408b3de68d39
aksrajvanshi <aksrajvanshi@gmail.com>
2019-11-18 08:57:34 -0500

    Added sorted results to rating and location based search

--- src/main/java/com/app/yumdrop/ServiceImplementation/RestaurantSearchServiceImpl.java
+++ src/main/java/com/app/yumdrop/ServiceImplementation/RestaurantSearchServiceImpl.java
@@ -16,10 +16,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 @Service
 public class RestaurantSearchServiceImpl implements RestaurantSearchService {
@@ -58,6 +55,8 @@ public class RestaurantSearchServiceImpl implements RestaurantSearchService {
             restaurantResultsWithDetails.add(res);
         }
 
+
+        restaurantResultsWithDetails.sort(Comparator.comparing(RestaurantSearchResults::getDistanceFromUserInMeters));
         return new ResponseEntity<>(restaurantResultsWithDetails, HttpStatus.OK);
     }
 
@@ -85,7 +84,7 @@ public class RestaurantSearchServiceImpl implements RestaurantSearchService {
             distanceBetweenAddressesCalculatorService.calculateDistance(userAddress, res);
             restaurantResultsWithDetails.add(res);
         }
-
+        restaurantResultsWithDetails.sort(Comparator.comparing(RestaurantSearchResults::getDistanceFromUserInMeters));
         return new ResponseEntity<>(restaurantResultsWithDetails, HttpStatus.OK);
     }
 
@@ -110,11 +109,44 @@ public class RestaurantSearchServiceImpl implements RestaurantSearchService {
             restaurantResultsWithDetails.add(res);
         }
 
+        restaurantResultsWithDetails.sort(Comparator.comparing(RestaurantSearchResults::getDistanceFromUserInMeters));
         return new ResponseEntity<>(restaurantResultsWithDetails, HttpStatus.OK);
     }
 
     @Override
-    public ResponseEntity<?> getRestaurantResultsByRatings(String userAddress) {
-        return null;
+    public ResponseEntity<?> getRestaurantResultsByRatings(String userAddress){
+
+        List<Restaurant> searchedRestaurant = (List<Restaurant>) restaurantRepository.findAll();
+        List<RestaurantSearchResults> restaurantResultsWithDetails = new ArrayList<>(searchedRestaurant.size());
+        if (searchedRestaurant.size() == 0) {
+            SuccessMessage searchResultsWithZeroRest = new SuccessMessage(new Date(), "No restaurants matched your query");
+            return new ResponseEntity<>(searchResultsWithZeroRest, HttpStatus.OK);
+        }
+
+        for (int i = 0; i < searchedRestaurant.size(); i++) {
+
+            RestaurantSearchResults res = new RestaurantSearchResults();
+
+            Restaurant currentRestaurant = searchedRestaurant.get(i);
+            res.setRestaurantDetails(currentRestaurant);
+            RestaurantRatings currentRestaurantRatings = restaurantRatingsRepository.findByrestaurantId(currentRestaurant.getRestaurantId());
+            res.setRestaurantRatings(currentRestaurantRatings);
+            distanceBetweenAddressesCalculatorService.calculateDistance(userAddress, res);
+            restaurantResultsWithDetails.add(res);
+        }
+
+        Collections.sort(restaurantResultsWithDetails, new Comparator<RestaurantSearchResults>() {
+            @Override
+            public int compare(RestaurantSearchResults u1, RestaurantSearchResults u2) {
+
+                double rating1 = u1.getRestaurantRatings().getOverallRating() / u1.getRestaurantRatings().getNumberOfUsers();
+                double rating2 = u2.getRestaurantRatings().getOverallRating() / u2.getRestaurantRatings().getNumberOfUsers();
+
+                return Double.compare(rating1, rating2);
+            }
+        });
+
+        return new ResponseEntity<>(restaurantResultsWithDetails, HttpStatus.OK);
+
     }
 }
