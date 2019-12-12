553568f2ef5ad5ac29e0bc2b3ced770d6666baa0
Akshay <aksrajvanshi@gmail.com>
2019-12-12 12:35:48 -0500

    new resources/static files + application.properties

---
+++ src/main/resources/static/precache-manifest.a882fa69c9e96ba904147c3798244fcc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3bc40a4f7553611078e75a94fed83cd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1d5f0631867f6a6f3deb",
+    "url": "/static/css/2.2eb84ce8.chunk.css"
+  },
+  {
+    "revision": "b3e33f7bce0fb349d684",
+    "url": "/static/css/main.ea71ccdf.chunk.css"
+  },
+  {
+    "revision": "1d5f0631867f6a6f3deb",
+    "url": "/static/js/2.df1ed41a.chunk.js"
+  },
+  {
+    "revision": "b3e33f7bce0fb349d684",
+    "url": "/static/js/main.fcc0571a.chunk.js"
+  },
+  {
+    "revision": "5ad6dc52180fca3143ed",
+    "url": "/static/js/runtime-main.a9e8376a.js"
+  }
+]);
