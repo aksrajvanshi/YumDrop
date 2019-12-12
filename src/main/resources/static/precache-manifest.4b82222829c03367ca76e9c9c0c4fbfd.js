0d78ccd3892fdf17819c5f6cdfbb86a2ddaff6f6
Akshay <aksrajvanshi@gmail.com>
2019-12-12 16:13:46 -0500

    adding resources static files

---
+++ src/main/resources/static/precache-manifest.4b82222829c03367ca76e9c9c0c4fbfd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba16d7a68faa81747588e59ee4289e24",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b458e17a18ac8cac94b9",
+    "url": "/static/css/2.2eb84ce8.chunk.css"
+  },
+  {
+    "revision": "e0dc55b0a7957dfc7ff8",
+    "url": "/static/css/main.cc8b38dc.chunk.css"
+  },
+  {
+    "revision": "b458e17a18ac8cac94b9",
+    "url": "/static/js/2.88b44b29.chunk.js"
+  },
+  {
+    "revision": "e0dc55b0a7957dfc7ff8",
+    "url": "/static/js/main.53b8cbc9.chunk.js"
+  },
+  {
+    "revision": "5ad6dc52180fca3143ed",
+    "url": "/static/js/runtime-main.a9e8376a.js"
+  }
+]);
